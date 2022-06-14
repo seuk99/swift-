56f962960576fb3a8f21426c47450c7472aadc88
seuk99 <106359725+seuk99@users.noreply.github.com>
2022-06-14 20:22:29 +0900

    Add files via upload

---
+++ memo/memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by 강민석 on 2022/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
